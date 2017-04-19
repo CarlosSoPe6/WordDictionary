c1d5892dfb09eed63fcdb6b29bbe99280d1758f8
Carlos Soto Pérez <carlos348@outlook.com>
2017-04-19 12:39:10 -0500

    Métodos base agregados

--- src/Dictionary.h
+++ src/Dictionary.h
@@ -2,9 +2,19 @@
  * Dictionary.h
  * TDA dictionary
  */
+
+#include "Text.h"
+
 #ifndef _H_DICT_
 #define _H_DICT_
 
 typedef struct srtDictionaryHead * Dictionary;
+
+Dictionary dict_create();
+void dict_destroy();
+unsigned int dict_size(Dictionary);
+void dict_set(Dictionary, Text, Text);
+Text dict_get(Dictionary, Text);
+Text * dict_keys(Dictionary);
 
 #endif
