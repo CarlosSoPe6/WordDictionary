0710050e092a9a6f5b37d93064bcd690041298cb
carlomunoz34 <carlo@sommusa.com.mx>
2017-04-20 11:36:13 -0500

    Terminada la funcion dict_set

--- src/Dictionary.c
+++ src/Dictionary.c
@@ -68,6 +68,7 @@ unsigned int dict_size(Dictionary d){
 
 void dict_set(Dictionary d, Text word, Text def){
     int i;
+    //Si se agragó el elemento, la bandera será falso
     bool flag = true;
     int cursor_pos = 0;
     char cursor_value = 0;
@@ -124,35 +125,189 @@ void dict_set(Dictionary d, Text word, Text def){
                 n = n->e;
                 break;
             case 'F':
-            if(n->f == NULL){
-                n->f->data->word = compare_word;
-                n->f->data->def = def;
-                flag = false;
-            }
-            n = n->f;
+                if(n->f == NULL){
+                    n->f->data->word = compare_word;
+                    n->f->data->def = def;
+                    flag = false;
+                }
+                n = n->f;
+                break;
+            case 'G':
+                if(n->g == NULL){
+                    n->g->data->word = compare_word;
+                    n->g->data->def = def;
+                    flag = false;
+                }
+                n = n->g;
+                break;
+            case 'H':
+                if(n->f == NULL){
+                    n->h->data->word = compare_word;
+                    n->h->data->def = def;
+                    flag = false;
+                }
+                n = n->h;
+                break;
+            case 'I':
+                if(n->i == NULL){
+                    n->i->data->word = compare_word;
+                    n->i->data->def = def;
+                    flag = false;
+                }
+                n = n->i;
+                break;
+            case 'J':
+                if(n->j == NULL){
+                    n->j->data->word = compare_word;
+                    n->j->data->def = def;
+                    flag = false;
+                }
+                n = n->j;
+                break;
+            case 'K':
+                if(n->k == NULL){
+                    n->k->data->word = compare_word;
+                    n->k->data->def = def;
+                    flag = false;
+                }
+                n = n->k;
+                break;
+            case 'L':
+                if(n->l == NULL){
+                    n->l->data->word = compare_word;
+                    n->l->data->def = def;
+                    flag = false;
+                }
+                n = n->l;
+                break;
+            case 'M':
+                if(n->m == NULL){
+                    n->m->data->word = compare_word;
+                    n->m->data->def = def;
+                    flag = false;
+                }
+                n = n->m;
+                break;
+            case 'N':
+                if(n->n == NULL){
+                    n->n->data->word = compare_word;
+                    n->n->data->def = def;
+                    flag = false;
+                }
+                n = n->n;
+                break;
+            case 'Ñ':
+                if(n->nacute == NULL){
+                    n->nacute->data->word = compare_word;
+                    n->nacute->data->def = def;
+                    flag = false;
+                }
+                n = n->nacute;
+                break;
+            case 'O':
+                if(n->o == NULL){
+                    n->o->data->word = compare_word;
+                    n->o->data->def = def;
+                    flag = false;
+                }
+                n = n->o;
+                break;
+            case 'P':
+                if(n->p == NULL){
+                    n->p->data->word = compare_word;
+                    n->p->data->def = def;
+                    flag = false;
+                }
+                n = n->p;
+                break;
+            case 'Q':
+                if(n->q == NULL){
+                    n->q->data->word = compare_word;
+                    n->q->data->def = def;
+                    flag = false;
+                }
+                n = n->q;
+                break;
+            case 'R':
+                if(n->r == NULL){
+                    n->r->data->word = compare_word;
+                    n->r->data->def = def;
+                    flag = false;
+                }
+                n = n->r;
+                break;
+            case 'S':
+                if(n->s == NULL){
+                    n->s->data->word = compare_word;
+                    n->s->data->def = def;
+                    flag = false;
+                }
+                n = n->s;
+                break;
+            case 'T':
+                if(n->t == NULL){
+                    n->t->data->word = compare_word;
+                    n->t->data->def = def;
+                    flag = false;
+                }
+                n = n->t;
+                break;
+            case 'U':
+                if(n->u == NULL){
+                    n->u->data->word = compare_word;
+                    n->u->data->def = def;
+                    flag = false;
+                }
+                n = n->u;
+                break;
+            case 'V':
+                if(n->v == NULL){
+                    n->v->data->word = compare_word;
+                    n->v->data->def = def;
+                    flag = false;
+                }
+                n = n->v;
+                break;
+            case 'W':
+                if(n->w == NULL){
+                    n->w->data->word = compare_word;
+                    n->w->data->def = def;
+                    flag = false;
+                }
+                n = n->w;
+                break;
+            case 'X':
+                if(n->x == NULL){
+                    n->x->data->word = compare_word;
+                    n->x->data->def = def;
+                    flag = false;
+                }
+                n = n->x;
+                break;
+            case 'Y':
+                if(n->y == NULL){
+                    n->y->data->word = compare_word;
+                    n->y->data->def = def;
+                    flag = false;
+                }
+                n = n->y;
+                break;
+            case 'Z':
+                if(n->z == NULL){
+                    n->z->data->word = compare_word;
+                    n->z->data->def = def;
+                    flag = false;
+                }
+                n = n->z;
+                break;
+            case '_':
+                if(n->underscore == NULL){
+                    n->underscore->data->word = compare_word;
+                    n->underscore->data->def = def;
+                    flag = false;
+                }
+                n = n->underscore;
             break;
-            case 'G':break;
-            case 'H':break;
-            case 'I':break;
-            case 'J':break;
-            case 'K':break;
-            case 'L':break;
-            case 'M':break;
-            case 'N':break;
-            case 'Ñ':break;
-            case 'O':break;
-            case 'P':break;
-            case 'Q':break;
-            case 'R':break;
-            case 'S':break;
-            case 'T':break;
-            case 'U':break;
-            case 'V':break;
-            case 'W':break;
-            case 'X':break;
-            case 'Y':break;
-            case 'Z':break;
-            case '_':break;
         }
         cursor_pos++;
     }
