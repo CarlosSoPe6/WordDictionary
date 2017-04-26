7121e652d208df97ad4819ab7537067f34b2482e
Carlos Soto Pérez <carlos348@outlook.com>
2017-04-26 11:26:17 -0500

    La palabra ya se buscaba

--- src/Dictionary.c
+++ src/Dictionary.c
@@ -341,11 +341,7 @@ Text dict_get(Dictionary dict, Text word) {
 		// Manejar que el nodo inicial es null
 		flag = FALSE;
 	}
-	else if (!text_compare(n->key, compare_word)) {
-		// La palabra está en la raiz
-		flag = FALSE;
-	}
-    
+
 	while (flag && text_compare(n->key, compare_word)) {
 		cursor_value = text_charAt(word, cursor_pos);
 		switch (cursor_value) {
@@ -441,8 +437,7 @@ Text dict_get(Dictionary dict, Text word) {
 	}
 	if (!flag) {
 		return_text = text_create("Palabra no encontrada");
-	}
-	else {
+	} else {
 		return_text = n->def;
 	}
 	
